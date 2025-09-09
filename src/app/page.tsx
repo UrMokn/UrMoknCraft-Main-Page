e24f798e69e67208330bf2d7099289d3ee20556f
UrMokn <urmoknmc@gmail.com>
2025-09-10 07:55:26 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,48 @@
-'use client';
-
 import { Icon } from '@/components/iconify-icon';
 import { links } from '@/lib/constants/links';
 import { Button } from '@nextui-org/button';
-import { Link } from '@nextui-org/link';
-import { cn } from '@nextui-org/theme';
 
 export default function Home() {
   return (
-    <div
-      className={cn(
-        'mx-auto max-w-3xl flex flex-col text-center gap-4 md:gap-6'
-      )}
-    >
-      <h1 className="w-full text-4xl md:text-5xl font-black tracking-tight">
-        Java&統合版クロスプレイ
-        <span className="bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent">
-          UrMoknCraft
-        </span>
-        Server
-      </h1>
-      <div className="flex justify-center gap-2">
-        <Button
-          as={Link}
-          href={links.abc}
-          color="primary"
-          isExternal
-        >
-          サーバーに参加
-        </Button>
-        <Button
-          as={Link}
-          href={links.abc}
-          isExternal
-        >
-          Discord
-        </Button>
-      </div>
-    </div>
-  );
-}
-
-  );
-}
-          as={Link}
-          href={links.abc}
-        //  startContent={<Icon icon="solar:settings-bold" className="text-[24px]" />}
-          isExternal
-        >
-          Discord
-        </Button>
-      </div>
-    </div>
-  );
+    <>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="mx-auto grid max-w-(--breakpoint-xl) px-4 py-8 text-center lg:py-16">
+          <div className="mx-auto place-self-center">
+            <h1 className="mb-4 max-w-2xl text-4xl leading-none font-extrabold tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+              Next.js Enterprise Boilerplate
+            </h1>
+            <p className="mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl dark:text-gray-400">
+              Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
+              Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
+              enjoyable development process.
+            </p>
+            <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
+              Get started
+            </Button>
+            <Button
+              href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+              intent="secondary"
+            >
+              Deploy Now
+            </Button>
+          </div>
+        </div>
+      </section>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="mx-auto max-w-(--breakpoint-xl) px-4 py-8 sm:py-16 lg:px-6">
+          <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
+            {LP_GRID_ITEMS.map((singleItem) => (
+              <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
+                <div className="bg-primary-100 dark:bg-primary-900 mb-4 flex size-10 items-center justify-center rounded-full p-1.5 text-blue-700 lg:size-12">
+                  {singleItem.icon}
+                </div>
+                <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
+                <p className="text-gray-500 dark:text-gray-400">{singleItem.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
