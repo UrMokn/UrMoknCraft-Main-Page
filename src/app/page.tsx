09271cd958ffd45a9b5c324a67aa0760e602ac07
UrMokn <urmoknmc@gmail.com>
2025-09-10 08:13:33 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { links } from '@/lib/constants/links';
 import { Button } from '@nextui-org/button';
 
 export default function Home() {
-  // ここで直接定義
   const LP_GRID_ITEMS = [
     {
       title: "高速開発",
@@ -32,8 +31,6 @@ export default function Home() {
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl dark:text-gray-400">
               Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
-              Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
-              enjoyable development process.
             </p>
             <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
               Get started
@@ -47,33 +44,29 @@ export default function Home() {
           </div>
         </div>
       </section>
+
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto max-w-(--breakpoint-xl) px-4 py-8 sm:py-16 lg:px-6">
           <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
             {LP_GRID_ITEMS.map((singleItem) => (
-              <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
+              <div
+                key={singleItem.title}
+                className="flex flex-col items-center justify-center text-center"
+              >
                 <div className="bg-primary-100 dark:bg-primary-900 mb-4 flex size-10 items-center justify-center rounded-full p-1.5 text-blue-700 lg:size-12">
                   {singleItem.icon}
                 </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
-                <p className="text-gray-500 dark:text-gray-400">{singleItem.description}</p>
+                <h3 className="mb-2 text-xl font-bold dark:text-white">
+                  {singleItem.title}
+                </h3>
+                <p className="text-gray-500 dark:text-gray-400">
+                  {singleItem.description}
+                </p>
               </div>
             ))}
           </div>
         </div>
       </section>
     </>
-  )
-}
-                  {singleItem.icon}
-                </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
-                <p className="text-gray-500 dark:text-gray-400">{singleItem.description}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-    </>
-  )
+  );
 }
